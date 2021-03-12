c5c30d783ec5d5a76e7da1f7c329006bdeda2be7
moazzamsyntax <moazzamsyntax@gmail.com>
2021-03-11 20:29:02 -0500

    new file git added

---
+++ src/testTheGit/git.java
@@ -0,0 +1,13 @@
+package testTheGit;
+
+public class git {
+    public static void main(String[] args) {
+
+
+        System.out.println("this is git");
+
+        System.out.println("we are learning git");
+
+
+    }
+}
