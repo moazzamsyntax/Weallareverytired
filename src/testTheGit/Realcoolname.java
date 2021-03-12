4374f8eeeb5b3465f059ace15cb16681d9944030
moazzamsyntax <moazzamsyntax@gmail.com>
2021-03-11 21:12:27 -0500

    take me to heaven

---
+++ src/testTheGit/Realcoolname.java
@@ -0,0 +1,16 @@
+package testTheGit;
+
+public class Realcoolname {
+
+    public static void main(String[] args) {
+
+
+                some code i am writing
+                standing in the dark
+                singing my song
+                holding my pen
+                making it all go away
+
+
+    }
+}
--- src/testTheGit/git.java
+++ src/testTheGit/git.java
@@ -9,5 +9,8 @@ public class git {
         System.out.println("we git");
 
 
+
+
+
     }
 }
